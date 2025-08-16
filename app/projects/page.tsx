02dbfb5eb0ef3f3d858539348397c682b3fdd7a2
Sanjana Yadav <126410251+SanjanaYdv1@users.noreply.github.com>
2025-08-16 16:17:44 +0545

    completed projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,76 +6,65 @@ import { useState } from "react";
 
 const clients = [
   {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa1.jpg",
-    name: "Bajeko Sekuwa, Kathmandu",
-    text: "Maintained a lush green lawn at a residential villa, ensuring every blade of grass stayed healthy through regular mowing, precise watering schedules, and nutrient-rich fertilizing to keep it vibrant all year.",
-    link: "https://www.bajekosekuwa.com/",
-  },
-  {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa2.jpg",
-    name: "The British Embassy, Kathmandu",
-    text: "Designed and planted a vibrant flower garden at a corporate office, combining seasonal blooms, creative color patterns, and low-maintenance greenery to provide a visually stunning and professional outdoor space.",
-    link: "https://www.gov.uk/world/organisations/british-embassy-kathmandu",
-  },
-  {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa3.jpg",
-    name: "Aloft Hotel, Thamel",
-    text: "Revitalized a backyard vegetable patch, enriching the soil with organic compost, planting fresh seedlings, and installing proper irrigation to ensure a consistent and healthy supply of homegrown produce.",
-    link: "https://www.marriott.com/en-us/hotels/ktmal-aloft-kathmandu-thamel/overview/?nst=paid&cid=PAI_GLB0004YFY_GLE000BHOR_GLF000OCGE&nst=paid&gclsrc=aw.ds&gad_source=1&gad_campaignid=22178666619&gbraid=0AAAAADilnidqVZJVxY377ZINfb9DlsduA&gclid=CjwKCAjw49vEBhAVEiwADnMbbB2ExULZAhvCxIRIOMKWNlEMTC0Sg2F8FSSBwc4FAzV_04N6yDk9qxoCpUIQAvD_BwE",
-  },
-  {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa4.jpg",
-    name: "Hotel Yak and Yeti",
-    text: "Installed an automatic irrigation system for a farmhouse, using water-saving technology, adjustable timers, and strategic placement to optimize coverage while maintaining plant health in all weather conditions.",
-    link: "https://www.yakandyeti.com/",
-  },
-  {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa5.jpg",
-    name: "Soltee Westend, Itahari",
-    text: "Landscaped a beachfront garden with tropical plants, carefully selecting salt-tolerant species, adding decorative pathways, and incorporating shaded seating areas to create a relaxing coastal retreat.",
-    link: "https://soaltee.com/",
-  },
-  {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa81.jpg",
-    name: "Metropolis Hotel",
-    text: "Pruned and shaped decorative hedges at a luxury hotel, creating artistic patterns, removing overgrowth, and enhancing curb appeal to maintain a polished and inviting outdoor environment for guests.",
-    link: "#",
-  },
-  {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa7.jpg",
-    name: "Mediciti Hospital",
-    text: "Created a rooftop herb garden for an apartment, using vertical planters, organic soil blends, and smart sun positioning to maximize growth while keeping fresh herbs within easy reach for daily use.",
-    link: "https://www.nepalmediciti.com/",
-  },
-  {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa82.jpg",
-    name: "Grande Hospital",
-    text: "Renovated a school playground with safe, eco-friendly greenery, replacing worn-out turf, planting soft grass varieties, and adding shaded plant areas to create a healthy and child-friendly outdoor learning space.",
-    link: "https://www.grandehospital.com/en",
-  },
-  {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa9.jpg",
-    name: "Mercure Hotel",
-    text: "Planted palm trees along a villa driveway, arranging them for symmetrical balance, adding decorative ground cover, and integrating lighting to highlight their beauty both day and night.",
-    link: "#",
-  },
-  {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa10.jpg",
-    name: "Hilton Hotel, Kathmandu",
-    text: "Restored a community park’s flower beds, removing old and faded plants, introducing fresh seasonal flowers, and enriching the soil to promote long-lasting, healthy blooms throughout the year.",
-    link: "https://www.hilton.com/en/",
-  },
-   {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa11.jpg",
-    name: "Hilton Hotel, Kathmandu",
-    text: "Added seasonal blooms to a shopping mall entrance, arranging vibrant floral displays, matching colors to brand themes, and ensuring a fresh, welcoming look for shoppers and visitors alike.",
-    link: "https://www.hilton.com/en/",
-  },
-   {
-    image: "/image/gallery/gardensewa-gallery/gardensewa-gallery/gardensewa12.jpg",
-    name: "Hilton Hotel, Kathmandu",
+    image: "/image/project/project1.jpg",
+    name: "Jorpati Project",
     text: "Built a Zen-style garden at a wellness retreat, incorporating smooth river stones, calming water features, and minimalistic greenery to create a peaceful space for relaxation and meditation.",
-    link: "https://www.hilton.com/en/",
+ 
+  },
+  {
+    image: "/image/project/project2.jpg",
+    name: "Baneshwor Project",
+    text: "Designed and planted a vibrant flower garden at a corporate office, combining seasonal blooms, creative color patterns, and low-maintenance greenery to provide a visually stunning and professional outdoor space.",
+  },
+  {
+    image: "/image/project/project3.jpg",
+    name: "Jawalakhel Project",
+    text: "Maintained a lush green lawn at a residential villa, ensuring every blade of grass stayed healthy through regular mowing, precise watering schedules, and nutrient-rich fertilizing to keep it vibrant all year.",
+  },
+  {
+    image: "/image/project/project4.jpg",
+    name: "Sano Thimi Project",
+    text: "Pruned and shaped decorative hedges at a luxury hotel, creating artistic patterns, removing overgrowth, and enhancing curb appeal to maintain a polished and inviting outdoor environment for guests.",
+  },
+  {
+    image: "/image/project/project5.jpg",
+    name: " Anamnagar Project",
+    text: "Installed an automatic irrigation system for a farmhouse, using water-saving technology, adjustable timers, and strategic placement to optimize coverage while maintaining plant health in all weather conditions.",
+  },
+  {
+    image: "/image/project/project6.jpg",
+    name: "Budhanilkantha Project",
+    text: "Restored a community park’s flower beds, removing old and faded plants, introducing fresh seasonal flowers, and enriching the soil to promote long-lasting, healthy blooms throughout the year.",
+  },
+  {
+    image: "/image/project/project7.jpg",
+    name: "Gathaghar Project",
+    text: "Revitalized a backyard vegetable patch, enriching the soil with organic compost, planting fresh seedlings, and installing proper irrigation to ensure a consistent and healthy supply of homegrown produce.",
+  },
+  {
+    image: "/image/project/project8.jpg",
+    name: "Dhulikhel Project",
+    text: "Renovated a school playground with safe, eco-friendly greenery, replacing worn-out turf, planting soft grass varieties, and adding shaded plant areas to create a healthy and child-friendly outdoor learning space.",
+  },
+  {
+    image: "/image/project/project9.jpg",
+    name: "Naikap Project",
+    text: "Created a rooftop herb garden for an apartment, using vertical planters, organic soil blends, and smart sun positioning to maximize growth while keeping fresh herbs within easy reach for daily use.",
+  },
+  {
+    image: "/image/project/project10.jpg",
+    name: "Kamalpokhari Project",
+    text: "Landscaped a beachfront garden with tropical plants, carefully selecting salt-tolerant species, adding decorative pathways, and incorporating shaded seating areas to create a relaxing coastal retreat.",
+  },
+   {
+    image: "/image/project/project11.jpg",
+    name: "Banepa Project",
+    text: "Planted palm trees along a villa driveway, arranging them for symmetrical balance, adding decorative ground cover, and integrating lighting to highlight their beauty both day and night.",
+  },
+   {
+    image: "/image/project/project12.jpg",
+    name: "Naxal Project",
+    text: "Added seasonal blooms to a shopping mall entrance, arranging vibrant floral displays, matching colors to brand themes, and ensuring a fresh, welcoming look for shoppers and visitors alike.",
   },
 ];
 
@@ -113,45 +102,45 @@ export default function ClientPage() {
 
             {/* Section description */}
             <p className="max-w-[858px] mb-8 text-base leading-relaxed md:text-lg text-white">
-              We believe that everyone deserves a little more green in their
-              life. Garden Sewa is built to bring plants, people, and peace
-              together through simple and thoughtful gardening services.
+              Showcasing our finest gardening and landscaping works — from home gardens to corporate spaces, each project is a step towards greener living.
             </p>
           </div>
         </div>
       </div>
       {/* Cards */}
-     <section className="mx-auto px-8 py-4 flex flex-col gap-6 mb-25 max-w-[1440px] flex-wrap">
-      <div className="flex flex-wrap justify-center items-center gap-4 sm:gap-6">
-        {clients.map((client, index) => (
-          <div
-            key={index}
-            className="bg-white border-[3px] border-[#C9EDC5] h-[427px] w-[290px] rounded-3xl flex flex-col justify-start items-start text-left relative overflow-hidden cursor-pointer"
-          >
-            {/* Image section */}
-            <div className="w-full flex justify-center h-80 items-center">
-              <img
-                src={client.image}
-                alt={client.name}
-                className="h-full w-full object-contain rounded-b-4xl"
-              />
-            </div>
+     <section className="px-4 lg:px-16 py-10 max-w-7xl mx-auto">
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+    {clients.map((client, index) => (
+      <div
+        key={index}
+        className="bg-white border-[2px] border-[#C9EDC5] shadow-md rounded-3xl overflow-hidden transform transition-transform hover:scale-105 hover:shadow-xl duration-300"
+      >
+        {/* Image section */}
+        <div className="w-full h-[220px] relative">
+          <img
+            src={client.image}
+            alt={client.name}
+            className="w-full h-full object-cover"
+            style={{
+              clipPath: 'ellipse(100% 85% at 50% 0%)',
+            }}
+          />
+        </div>
 
-            {/* Content section */}
-            <div className="flex w-full flex-col mt-0 p-8 sm:p-6">
-              <div className="flex flex-col items-start w-full">
-                <h3 className="text-xl sm:text-2xl font-semibold mb-2 text-black">
-                  {client.name}
-                </h3>
-                <p className="text-xs sm:text-sm leading-relaxed text-gray-600 mb-4">
-                  {client.text}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))}
+        {/* Content section */}
+        <div className="px-6 py-5 flex flex-col">
+          <h3 className="text-lg md:text-xl font-semibold text-black mb-2">
+            {client.name}
+          </h3>
+          <p className="text-sm text-gray-600 leading-relaxed">
+            {client.text}
+          </p>
+        </div>
       </div>
-    </section>
+    ))}
+  </div>
+</section>
+
       {/*image bg */}
       <div className="mb-30 py-12">
         <div
@@ -175,7 +164,7 @@ export default function ClientPage() {
               Join our growing network of satisfied clients.
             </p>
             <Link href="book-a-service">
-              <button className="bg-gradient-to-l from-[#009000] via-[#009800] to-[#00C400] hover:from-[#007000] hover:via-[#007800] hover:to-[#00A400] text-white font-semibold px-6 py-2 md:px-8 md:py-3 rounded-lg hover:bg-secondary transition">
+              <button className="bg-gradient-to-l from-[#009000] via-[#009800] to-[#00C400] hover:from-[#007000] hover:via-[#007800] hover:to-[#00A400] text-white font-semibold px-6 py-2 md:px-8 md:py-3 rounded-4xl hover:bg-secondary transition">
                 Become a client
               </button>
             </Link>
